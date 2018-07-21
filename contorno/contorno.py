ea72c33cf8c4101a04286f3021c9ec0cc4c11b36
Leonardo Souza <leo@example.com>
2018-07-21 20:42:05 +0100

    updates

--- contorno/contorno.py
+++ contorno/contorno.py
@@ -13,11 +13,32 @@ def save(img):
 
 def extrai():
     imgGray = cv2.imread('t_0.jpg', -1)
+    arredaContorno(imgGray)
     save(imgGray)
 
 def arredaContorno(img):
+    shape = img.shape
+    novaMat = np.zeros(shape, dtype = "uint8")
+
+    im2, contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = removeContornosPqnos(contours)
+    print(len(contours))
+
     
+def removeContornosPqnos(cnts):
+    retorno = []
+    totalRemovidos = 0
+    for i,c in enumerate(cnts):
+        if cv2.contourArea(c) > 100:
+            retorno.append(c)
+            totalRemovidos+=1
+
+    print('Total removidos: ' + str(totalRemovidos))
+    return retorno
+
 
 
 if __name__ == '__main__':
     extrai()
+
+
